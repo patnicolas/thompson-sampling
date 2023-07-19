036b6a4546daa28e189a814811bd824cfd78b0e8
PatNicolas <patricknicolas@Patricks-MacBook-Pro-2.local>
2023-07-18 20:08:36 -0700

    0.1 Added comments

--- src/main/scala/org/scalaml/plots/LinePlot.scala
+++ src/main/scala/org/scalaml/plots/LinePlot.scala
@@ -1,3 +1,15 @@
+/**
+ * Copyright 2020 Patrick R. Nicolas. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ */
+
 package org.scalaml.plots
 
 import java.awt.{BasicStroke, Color, Shape, Stroke}
@@ -18,8 +30,6 @@ import Plot._
  * @param legend  legend for the plot of type '''PlotInfo'''
  * @param theme  Configuration for the display of plots of type '''PlotTheme'''
  * @author Patrick Nicolas
- * @since  0.97 November 18, 2013
- * @version 0.97
  * @see Scala for Machine Learning''Appendix'' Visualization
  * @see http://www.jfree.org
  */
